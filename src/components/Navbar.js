92b179e3df9f7290af19a368cb5fbb3d27a5660a
Bilal Khoukhi <hello@bilalkhoukhi.com>
2018-06-19 08:52:21 -0500

    Remove Github icon
    test commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,9 +29,6 @@ const Navbar = () => (
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
         </a>
       </div>
     </div>
